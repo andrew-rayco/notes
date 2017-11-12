7d0c1a09492eb50c5e6726b690737eff27ae9903
Andrew <andy@andycondon.com>
2017-11-13 01:44:13 +1300

    more refactoring

--- js/todo.js
+++ js/todo.js
@@ -1,28 +1,30 @@
 $(function() {
 
   // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyAdOnpc1Ls2U4h-reqvtghjKjA-rOaVLDM",
-      authDomain: "notes-6cd93.firebaseapp.com",
-      databaseURL: "https://notes-6cd93.firebaseio.com",
-      projectId: "notes-6cd93",
-      storageBucket: "",
-      messagingSenderId: "193360239634"
-    };
-    firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyAdOnpc1Ls2U4h-reqvtghjKjA-rOaVLDM",
+    authDomain: "notes-6cd93.firebaseapp.com",
+    databaseURL: "https://notes-6cd93.firebaseio.com",
+    projectId: "notes-6cd93",
+    storageBucket: "",
+    messagingSenderId: "193360239634"
+  }
+  firebase.initializeApp(config)
 
-  var $newItemForm = $('#newItemForm');
-  var $textInput = $('input:text');
-  var $form = $('form');
-  var emptyItemText = '<p class="error">Heyyyy! No empty items on the list please.</p>';
-  var fiveItemsText = '<p class="error">Ooooo. That\'s five items. Niiice.</p>';
-  var $fiveMsg = $($.parseHTML(fiveItemsText));
-  var $errorMsg = $($.parseHTML(emptyItemText));
-  var $listItems = $('li');
-  var $listItemText = $('li a').text;
+
+  var database = firebase.database()
+
+  var $newItemForm = $('#newItemForm')
+  var $textInput = $('input:text')
+  var $form = $('form')
+  var emptyItemText = '<p class="error">Heyyyy! No empty items on the list please.</p>'
+  var fiveItemsText = '<p class="error">Ooooo. That\'s five items. Niiice.</p>'
+  var $fiveMsg = $($.parseHTML(fiveItemsText))
+  var $errorMsg = $($.parseHTML(emptyItemText))
+  var $listItems = $('li')
+  var $listItemText = $('li a').text
   var todosArray = []
   var entryToBeUpdated, entryText
-  var database = firebase.database();
 
   database.ref('list').on('value', function(snapshot) {
     // hide loading icon and insult
@@ -38,7 +40,7 @@ $(function() {
 
     // generate li's for each list item
     for (var key in allTodos) {
-      $('ul').prepend('<li class="todo-item"><a id="' + allTodos[key].id + '" href="#">x</a><span>' + allTodos[key].item + '</span></li>');
+      $('ul').prepend('<li class="todo-item"><a id="' + allTodos[key].id + '" href="#">x</a><span>' + allTodos[key].item + '</span></li>')
     }
   }
 
@@ -46,9 +48,9 @@ $(function() {
   function addItem() {
     // Add new item to list
     $newItemForm.on('submit', function(e) {
-      e.preventDefault();
+      e.preventDefault()
 
-      var newText = $textInput.val();
+      var newText = $textInput.val()
 
       // If input isn't empty and not currently editing existing item, add new item
       if (newText !== '' && $('#addButton').val() !== 'Edit') {
@@ -58,7 +60,7 @@ $(function() {
         }
 
         todosArray.push(newText)
-        $textInput.val('');
+        $textInput.val('')
         addItemToDb(newText)
 
       } else if ($('#addButton').val() === 'Edit') {
@@ -70,80 +72,71 @@ $(function() {
 
       } else {
 
-        var $newMsg = $($errorMsg).hide().fadeIn(1000);
-        $form.prepend($newMsg);
-      };
+        var $newMsg = $($errorMsg).hide().fadeIn(1000)
+        $form.prepend($newMsg)
+      }
 
       // Surprise five list items message
       if ($('li').length == 5) {
-        $form.prepend($fiveMsg.hide().fadeIn(800).delay(1000).fadeOut(500));
+        $form.prepend($fiveMsg.hide().fadeIn(800).delay(1000).fadeOut(500))
       }
 
       if ($('li').length >= 1) {
-        $('ul').css("margin-bottom", "5%");
+        $('ul').css("margin-bottom", "5%")
       }
-    });
+    })
 
     // Make 'x's remove list item when clicked
     $('ul').on('click', '.todo-item>a', function(e) {
-      e.preventDefault();
-      $(this).parent().fadeOut(400);
+      e.preventDefault()
+      console.log(e)
+      $(this).parent().fadeOut(400)
       database.ref('list/' + e.target.id).remove()
       console.log(e.target.id)
-    });
+    })
 
-  };
-  addItem();
+  }
+  addItem()
 
   function addItemToDb(newListItem) {
     // Get a key for a new Post.
-    var newPostKey = firebase.database().ref().child('posts').push().key;
+    var newPostKey = database.ref().child('list').push().key
+    console.log(newPostKey)
+
     var singleItem = {
       item: newListItem,
-      id: newPostKey,
-    };
+      id: newPostKey
+    }
 
     database.ref('list/' + newPostKey).set(singleItem)
 
-    var ref = database.ref('list/' + newPostKey)
-    ref.on('value', gotData, errData)
+    // To access db item (remove '+ newPostKey' to view all items)
+    // var ref = database.ref('list/' + newPostKey)
+    // ref.on('value', gotData, errData)
   }
 
-  function gotData(data) {
-    console.log(data.val())
-  }
-
-  function errData(err) {
-    console.log('Error!')
-    console.log(err)
-  }
+  // Callbacks for ref.on above to read db items 
+  // function gotData(data) {
+  //   console.log(data.val())
+  // }
+  //
+  // function errData(err) {
+  //   console.log('Error!')
+  //   console.log(err)
+  // }
 
   function editItem(id, updatedItem) {
     database.ref('list/' + id).update({item: updatedItem, id: id})
   }
 
+  // Check if string is a url (and a valid one)
   function validUrl(str) {
-    var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/
     if (!regex.test(str)) {
-      return false;
+      return false
     } else {
-      return true;
+      return true
     }
-  }
-
-  // Sort items using jQuery UI
-  var sortEventHandler = function(event, ui) {
-    console.log(event)
-    console.log(ui)
-
-    var arrayToBeSorted = $('a')
-    var listValues = []
-
-    for (var i = 0; i < arrayToBeSorted.length; i++) {
-      listValues.push(arrayToBeSorted[i].id)
-      console.log(arrayToBeSorted[i].id)
-    }
-    console.log(listValues)
   }
 
   // edit entries
@@ -166,4 +159,4 @@ $(function() {
     $('#addButton').val('Edit')
   }
 
-});
+})
